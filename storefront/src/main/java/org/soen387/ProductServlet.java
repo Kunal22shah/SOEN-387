35e178fab2648a09f71db4f5de3a9fa259861366
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-10-03 12:43:29 -0400

    Adding the GET/products/:slug request

--- storefront/src/main/java/org/soen387/ProductServlet.java
+++ storefront/src/main/java/org/soen387/ProductServlet.java
@@ -11,25 +11,47 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-@WebServlet("/products")
+@WebServlet("/products/*")
 public class ProductServlet extends HttpServlet {
 
     private static StorefrontFacade store = new StorefrontFacade();
 
+    //Initialization method used for testing purposes
+    //This will create products in the store to test the GET request
+    //This method will only be executed once
+    public void init() throws ServletException{
+        super.init();
+        store.createProduct("1","test");
+        store.createProduct("2","test2");
+        store.updateProduct("2",  "test2", "test description", "test", "test-product", 25.99);
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Handle GET requests to /products
+        // Handle GET requests to /products/*
         response.setContentType("application/json");
 
-        //For testing purposes, initially there is no products in the store
-        //This is a test to see if its shows all the products if we explicitly add products in the store
-        //store.createProduct("1","test");
-        //store.createProduct("2","test2");
+        //GET path info from request (anything after "/products")
+        String getPathInfo = request.getPathInfo();
+
         Gson gson = new Gson();
         PrintWriter out = response.getWriter();
-        ArrayList<Product> allProducts = new ArrayList<Product>();
-        allProducts = store.getAllProduct();
-        String allProductsjson = gson.toJson(allProducts);
-        out.println(allProductsjson);
+
+        //Handle the GET /products request
+        if (getPathInfo == null){
+            ArrayList<Product> allProducts = new ArrayList<Product>();
+            allProducts = store.getAllProduct();
+            String allProductsjson = gson.toJson(allProducts);
+            out.println(allProductsjson);
+            return;
+        }
+
+        //Handle the GET /products/:slug request
+        //Extract slug form "/:slug"
+        String getRequestSlug = getPathInfo.split("/")[1];
+
+        String singleProduct = gson.toJson(store.getProductBySlug(getRequestSlug));
+        out.println(singleProduct);
+
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Handle POST requests to /products/:slug
