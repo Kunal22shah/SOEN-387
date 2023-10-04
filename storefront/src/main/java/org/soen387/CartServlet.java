9f316264549a1d170fc3b24bbae26cbfcafccab4
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2023-10-04 12:22:43 -0400

    added delete and download endpoints

--- storefront/src/main/java/org/soen387/CartServlet.java
+++ storefront/src/main/java/org/soen387/CartServlet.java
@@ -1,11 +1,15 @@
 package org.soen387;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.soen387.ProductServlet.store;
+
+@WebServlet("/cart/*")
 public class CartServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -14,5 +18,21 @@ public class CartServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Handle POST requests to /cart/products/:slug
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+        // Handle DELETE requests to /cart/products/:slug
+
+        String getPathInfo = request.getPathInfo();
+        if (getPathInfo == null || getPathInfo.split("/").length <= 1) {
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        String slug = getPathInfo.split("/")[1];
+        String user = "defaultUser";
+        store.removeProductFromCart(user, slug);
+//        response.setStatus(HttpServletResponse.SC_OK);
+
     }
 }
