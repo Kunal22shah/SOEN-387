1eed9d744cf5072426ab28907bc22c25eba8be5a
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-10-31 16:02:45 -0400

    Adding getOrders(user) and getOrder(user,id) functions

--- storefront/src/main/java/org/soen387/Order.java
+++ storefront/src/main/java/org/soen387/Order.java
@@ -2,13 +2,14 @@ package org.soen387;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Order {
-    private static int counter = 9999;
+    private static int counter = 10000;
     private int orderID;
     private String shippingAdress;
     private int trackingNumber;
-
+    private String user;
     private List<Product> orderProducts;
 
     public Order(){
@@ -16,13 +17,18 @@ public class Order {
         this.orderProducts = new ArrayList<>();
     }
 
-    public Order(String shippingAdress, int trackingNumber, List<Product> orderProducts){
+    public Order(String shippingAdress, List<Product> orderProducts, String user){
+        Random randomClass = new Random();
+
         this.orderID = counter++;
         this.shippingAdress = shippingAdress;
         this.orderProducts = orderProducts;
-        this.trackingNumber = trackingNumber;
+        this.trackingNumber = randomClass.nextInt(100000);
+        this.user = user;
     }
-
+    public String getUser() {
+        return user;
+    }
     public int getOrderID() {
         return orderID;
     }
