616ee8bfe33530fe3e383a47bb27418a02667258
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2023-11-01 18:19:39 -0400

    added Cart table to the DB

--- storefront/src/main/java/org/soen387/Cart.java
+++ storefront/src/main/java/org/soen387/Cart.java
@@ -6,46 +6,70 @@ import java.util.stream.Collectors;
 
 public class Cart {
 
-    private List<Product> products;
+    private List<CartItem> cartItems;
 
     public Cart() {
-        this.products = new ArrayList<>();
+        this.cartItems = new ArrayList<>();
     }
 
-    public List<Product> getProducts() {
-        return products;
+    public List<CartItem> getCartItems() {
+        return cartItems;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
     }
 
-    public void addProduct(Product p) {
+    public void addProduct(Product p, int quantity) {
         if (p != null) {
-
-            for (Product product : products) {
-                if (product.getSku().equals(p.getSku())) {
+            for (CartItem cartItem : cartItems) {
+                if (cartItem.getProduct().getSku().equals(p.getSku())) {
+                    cartItem.setQuantity(cartItem.getQuantity() + quantity);
                     return;
                 }
             }
-            products.add(p);
+            cartItems.add(new CartItem(p, quantity));
         }
-
     }
 
     public void removeProductBySku(String sku) {
-        products = products.stream()
-                .filter(product -> !product.getSku().equals(sku))
+        cartItems = cartItems.stream()
+                .filter(cartItem -> !cartItem.getProduct().getSku().equals(sku))
                 .collect(Collectors.toList());
-
     }
 
     public boolean containsProduct(String sku) {
-        for (Product product : products) {
-            if (product.getSku().equals(sku)) {
+        for (CartItem cartItem : cartItems) {
+            if (cartItem.getProduct().getSku().equals(sku)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static class CartItem {
+        private Product product;
+        private int quantity;
+
+        public CartItem(Product product, int quantity) {
+            this.product = product;
+            this.quantity = quantity;
+        }
+
+        public Product getProduct() {
+            return product;
+        }
+
+        public void setProduct(Product product) {
+            this.product = product;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
     }
 }
