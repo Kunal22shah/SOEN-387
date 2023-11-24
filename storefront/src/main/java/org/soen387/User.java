2ef152e349c3d4df16c08e4293bbda1bc2e0ee5a
Vatsa Shah <vatsashah45@gmail.com>
2023-11-24 15:49:56 -0500

    missing semicolon

--- storefront/src/main/java/org/soen387/User.java
+++ storefront/src/main/java/org/soen387/User.java
@@ -12,7 +12,7 @@ public class User {
     private String username;
     private String password;
     private String email;
-    private Role role
+    private Role role;
 
     public User() {
         this.role = Role.CUSTOMER;
